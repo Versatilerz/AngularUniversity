3ebc7a08290ddc4dcc3e404d28b677cf51169140
Nils van Gestel <nvangestel@yellowtail.nl>
2025-01-02 21:56:08 +0100

    Section 2 setup

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -5,8 +5,16 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-course';
+
+  onButtonClick() {
+    alert('Clicked the button');
+  }
+
+  onKeyUp(newTitle: string) {
+    this.title = newTitle;
+  }
 }
