eaed99eebe91d628a188c0ac52eb48d4724d2b9b
Nils van Gestel <nvangestel@yellowtail.nl>
2025-01-03 14:43:53 +0100

    pipes

--- angular-course-1-components/src/app/app.component.ts
+++ angular-course-1-components/src/app/app.component.ts
@@ -12,6 +12,13 @@ export class AppComponent {
   courses = COURSES;
   emptyList = [];
 
+  startDate = new Date(2000, 0, 1);
+  title = this.courses[0].description;
+  price = 9.99;
+  rate = 0.49;
+
+  course = COURSES[0];
+
   onCourseSelected(course: Course) {
     console.log("Emitted the following course....->", course);
   }
