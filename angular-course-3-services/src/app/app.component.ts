c8f1f1fcee939dae8581956456f94ec5864e7491
Nils van Gestel <nvangestel@yellowtail.nl>
2025-01-08 10:10:41 +0100

    coursesservice

--- angular-course-3-services/src/app/app.component.ts
+++ angular-course-3-services/src/app/app.component.ts
@@ -12,7 +12,8 @@ import { Course } from "./model/course";
 import { CourseCardComponent } from "./course-card/course-card.component";
 import { HighlightedDirective } from "./directives/highlighted.directive";
 import { Observable } from "rxjs";
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { CoursesService } from "./services/courses.service";
 
 @Component({
   selector: "app-root",
@@ -23,10 +24,15 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 export class AppComponent implements OnInit {
   courses$: Observable<Course[]>;
 
-  constructor(private http: HttpClient) {}
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
-    const params = new HttpParams().set("page", "1").set("pageSize", "10");
-    this.courses$ = this.http.get<Course[]>("/api/courses", { params });
+    this.courses$ = this.coursesService.loadCourses();
+  }
+
+  save(course: Course) {
+    this.coursesService
+      .saveCourse(course)
+      .subscribe(() => console.log("course got saved"));
   }
 }
